278c38985729bbb9bd5f3aa26e94d7c330b2eae8
Alberto Pinilla Gonzalez-Mohino <116631543+MetaCryptDev@users.noreply.github.com>
2024-02-29 16:18:46 +0100

    tranversal

--- PROYECTO_TRANSVERSAL_MANUEL_ALBERTO/src/main/java/com/transversal/demo/DAO/IDAOCarrera.java
+++
@@ -1,9 +0,0 @@
-package com.transversal.demo.DAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.transversal.demo.Domain.Entities.CarrerasEntity;
-
-public interface IDAOCarrera extends CrudRepository<CarrerasEntity, Integer> {
-
-}
--- PROYECTO_TRANSVERSAL_MANUEL_ALBERTO/src/main/java/com/transversal/demo/DAO/IDAOGalgos.java
+++
@@ -1,9 +0,0 @@
-package com.transversal.demo.DAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.transversal.demo.Domain.Entities.GalgosEntity;
-
-public interface IDAOGalgos extends CrudRepository<GalgosEntity, Integer> {
-
-}
